a26f736dde73e37c4d1552b9df472f5804893d1f
jan.weinschenker@googlemail.com <jan.weinschenker@googlemail.com@6d3d42d1-0307-d6f7-eb59-8c44da1dd0be>
2010-06-14 06:55:28 +0000

    removed @Inherited
    git-svn-id: https://weinschenker-jee-tutorial.googlecode.com/svn/trunk@14 6d3d42d1-0307-d6f7-eb59-8c44da1dd0be

--- demowebapp/src/main/java/org/weinschenker/demowebapp/cache/MyCache.java
+++ demowebapp/src/main/java/org/weinschenker/demowebapp/cache/MyCache.java
@@ -4,7 +4,6 @@
 package org.weinschenker.demowebapp.cache;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -15,7 +14,6 @@ import java.lang.annotation.Target;
  */
 @Target( { ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
-@Inherited
 public @interface MyCache {
 	
 	String cacheName();
